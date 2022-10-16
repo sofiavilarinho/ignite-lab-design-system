afa53e1b51e2b43c40598b8122787e37d1fba61e
sofiavilarinho <sof.vilarinho@gmail.com>
2022-10-16 21:46:46 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './styles/global.css'
+
+export function App() {
+
+  return (
+    <>
+    <h1 className='text-violet-500 font-bold text-5xl'>Hello World</h1>
+    
+    <button className='bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600'>
+      Enviar
+    </button>
+    
+    </>
+    
+      
+  )
+}
